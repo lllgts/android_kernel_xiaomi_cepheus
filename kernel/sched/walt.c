c8ae5bfbb963ca0d53e9cbd04082bd7281901346
lllgts <wuer031@126.com>
2023-03-11 12:50:38 +0000

    Revert "sched/walt: Improve the scheduler"
    This reverts commit fd48a83dbc8aac29809688a59f0c784575f55fab.

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -3711,22 +3711,6 @@ unlock:
 	return ret;
 }
 
-void sched_set_refresh_rate(enum fps fps)
-{
-	int new_nr_ticks;
-
-	if (HZ == 250) {
-		if (fps > FPS90)
-			new_nr_ticks = 2;
-		else if (fps == FPS90)
-			new_nr_ticks = 3;
-		else
-			new_nr_ticks = 5;
-		sched_window_nr_ticks_change(new_nr_ticks);
-	}
-}
-EXPORT_SYMBOL(sched_set_refresh_rate);
-
 /* Migration margins */
 unsigned int sysctl_sched_capacity_margin_up[MAX_MARGIN_LEVELS] = {
 			[0 ... MAX_MARGIN_LEVELS-1] = 1078}; /* ~5% margin */
@@ -3831,4 +3815,4 @@ unlock_mutex:
 
 	return ret;
 }
-#endif
+#endif)
